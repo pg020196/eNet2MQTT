e0a83067a4b61657da369aee0532d383a7acc038
Thomas Hogema <thomashogema@yahoo.nl>
2020-02-19 21:19:05 +0100

    Revert channelArray option in HASS

--- enet2mqtt/run.sh
+++ enet2mqtt/run.sh
@@ -8,8 +8,7 @@ CONFIG_PATH=/data/options.json
 ENET_IP="$(jq --raw-output '.enet_ip' $CONFIG_PATH)"
 MQTT_IP="$(jq --raw-output '.mqtt_ip' $CONFIG_PATH)"
 LOG_LEVEL="$(jq --raw-output '.log_level' $CONFIG_PATH)"
-CHANNEL_ARRAY="$(jq --raw-output '.channelArray' $CONFIG_PATH)"
 
 
 echo Starting node application
-node index.js -e $ENET_IP -m $MQTT_IP -v $LOG_LEVEL -c $CHANNEL_ARRAY
+node index.js -e $ENET_IP -m $MQTT_IP -v $LOG_LEVEL
